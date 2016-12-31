d232c90c2e7ffdb3f478ff5df41cb41d3e850ba8
jeiger <cagojeiger@naver.com>
2016-12-31 20:39:59 +0900

    first commit

---
+++ basic form/sub_main.cpp
@@ -0,0 +1,25 @@
+#include "sub_main.hpp"
+#include <stdio.h>
+
+
+void traing_random_weight(char* input_name,char* output_name)
+{
+  printf("traing_random_weight in : %s\n",input_name);
+  printf("traing_random_weight out : %s\n",output_name);
+}
+void traing_sellected_weight(char* input_name,char* output_name,char* weight_name)
+{
+  printf("traing_sellected_weight in : %s\n",input_name);
+  printf("traing_sellected_weight out : %s\n",output_name);
+  printf("traing_sellected_weight weight : %s\n",weight_name);
+}
+void mapping(char* input_name,char* output_name,char* weight_name)
+{
+  printf("mapping in : %s\n",input_name);
+  printf("mapping : %s\n",output_name);
+  printf("mapping : %s\n",weight_name);
+}
+void setting()
+{
+  printf("setting\n");
+}
