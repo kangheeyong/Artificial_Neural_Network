2fe614d8b2791103b08ff1147a522875f01579d5
jeiger <cagojeiger@naver.com>
2017-01-05 14:03:11 +0900

    chane dir name

--- Self Organizing Map/sub_main.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __SUB_MAIN_CPP__
-#define __SUB_MAIN_CPP__
-
-void traing_random_weight(char* input_name,char* output_name);
-void traing_sellected_weight(char* input_name,char* output_name,char* weight_name);
-void mapping(char* input_name,char* output_name,char* weight_name);
-void setting();
-#endif
